11f02bf08024fbef0ee2be14ff9b55a2c68436d5
Arsen Stepanyan <arsen0017@gmail.com>
2022-08-27 21:01:55 +0400

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://shoutcart.com/uploads/gig/bd6f3a23cd5294a2370b09bc413f3331.png' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://bipbap.ru/wp-content/uploads/2021/09/1577348890_17-730x380.jpg' />
+            </div>
+            <div>
+                avatar + discripshen
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
